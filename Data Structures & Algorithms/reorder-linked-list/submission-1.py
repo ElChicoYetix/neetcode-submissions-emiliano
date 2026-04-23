c2c9da98d294149ff3c6aff600d98532e117a0b2
Emiliano Rodríguez <111471072+ElChicoYetix@users.noreply.github.com>
2026-04-22 23:37:32 -0600

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.py
@@ -0,0 +1,47 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        S, F = head, head.next
+        while F and F.next:# is not null
+            S = S.next
+            F = F.next.next
+
+        second = S.next # inicio de la segunda mitad
+        S.next = None # Valor final / haces el split
+
+        # Reverse Linked List
+        """
+        prev, curr = None, head 
+        while curr: 
+            nxt = curr.next 
+            curr.next = prev 
+            prev = curr 
+            curr = nxt 
+        """
+
+        prev = None
+
+        while second:
+            nxt = second.next
+            second.next = prev 
+            prev = second 
+            second = nxt 
+
+        # Al final del while, second = None, quiero que sea el final, el tail, por eso prev
+        second = prev
+        first = head
+
+        # Juntar ambas listas. Second half might be shorter
+        while second:
+            tmp1, tmp2 = first.next, second.next # es como el nxt = curr.next 
+            first.next = second
+            second.next = tmp1
+
+            first, second = tmp1, tmp2
+        
+        return None
