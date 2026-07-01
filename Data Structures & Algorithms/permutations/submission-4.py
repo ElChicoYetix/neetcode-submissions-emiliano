2e7faaa932f4241319cf4ec2ef242e80cb27b20b
Emiliano Rodríguez <111471072+ElChicoYetix@users.noreply.github.com>
2026-07-01 12:42:52 -0600

    Add: permutations - submission-4

---
+++ Data Structures & Algorithms/permutations/submission-4.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perms = [[]]
+
+        for n in nums: # for every number, add to all existing permutations
+            new_perms = []
+
+            for p in perms: # go through every existing permutation
+
+                for i in range(len(p) + 1): # insert each element into this permutation
+                    p_copy = p.copy()
+                    p_copy.insert(i, n)
+                    new_perms.append(p_copy)
+
+            perms = new_perms
+
+        return perms
